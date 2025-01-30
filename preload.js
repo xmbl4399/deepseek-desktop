89372d9a4adef7e63643cda3b696e6661ea1c451
Alyetama <56323389+Alyetama@users.noreply.github.com>
2025-01-30 14:10:17 -0500

    Keep the app state after closing the window

--- preload.js
+++ preload.js
@@ -24,3 +24,20 @@ contextBridge.exposeInMainWorld('electron', {
     return null;
   }
 });
+
+window.addEventListener('beforeunload', () => {
+    const state = {
+        url: window.location.href,
+        scrollPosition: window.scrollY,
+    };
+    localStorage.setItem('appState', JSON.stringify(state));
+});
+
+window.addEventListener('load', () => {
+    localStorage.clear();
+    const savedState = JSON.parse(localStorage.getItem('appState'));
+    if (savedState) {
+        window.location.href = savedState.url;
+        window.scrollTo(0, savedState.scrollPosition);
+    }
+});
